605100a6e5b30c01ca1482a426a126750465bf53
ahmet cakan <ahmetcakan@gmx.net>
2025-03-07 11:06:37 +0100

    with lewagon api

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let taxiFareApiUrl = 'http://taxifare.ckna.net:8000/predict'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare-155597554769.europe-west1.run.app/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://taxifare.lewagon.com')) {
